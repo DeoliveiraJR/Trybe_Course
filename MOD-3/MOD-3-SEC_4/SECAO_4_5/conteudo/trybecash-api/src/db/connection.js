0acd6d7b0a5269800f821a28a0621a4234442adb
DeOliveira_JR <deoliveirajr1@gmail.com>
2023-03-15 11:39:09 -0300

    :gear: feat: conteudo MOD-3_SEC-4-D5
    :speaking_head: configurando variaveis de ambiente do app trybecash
    
    Refs: #MOD3

--- MOD-3/MOD-3-SEC_4/SECAO_4_5/conteudo/trybecash-api/src/db/connection.js
+++ MOD-3/MOD-3-SEC_4/SECAO_4_5/conteudo/trybecash-api/src/db/connection.js
@@ -1,16 +1,18 @@
 // src/db/connection.js
 
+// ⚠️ Atenção: toda vez que uma variável de ambiente é alterada, é necessário parar e reiniciar a aplicação, mesmo quando se utiliza o nodemon, pois estas variáveis de ambiente, por serem temporárias, são “carregadas” uma única vez no momento em que a aplicação é executada.
+
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  port: 33060,
-  user: 'root',
-  password: 'root',
-  database: 'trybecashdb',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
+  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
 });
 
 module.exports = connection;
--- MOD-3/MOD-3-SEC_4/SECAO_4_5/conteudo/trybecash-api/src/server.js
+++ MOD-3/MOD-3-SEC_4/SECAO_4_5/conteudo/trybecash-api/src/server.js
@@ -5,7 +5,7 @@ const connection = require('./db/connection');
 const PORT = 3001;
 
 app.listen(PORT, async () => {
-  console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 
   // O código abaixo é para testarmos a comunicação com o MySQL
   const [result] = await connection.execute('SELECT 1');
