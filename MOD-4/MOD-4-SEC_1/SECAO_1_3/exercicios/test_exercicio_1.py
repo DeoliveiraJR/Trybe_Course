5fabfafa1d9add6144d4d3ca545361e5817105f3
Francisco Fernandes de Oliveira Junior <92826873+DeoliveiraJR@users.noreply.github.com>
2023-05-04 18:27:17 +0000

    :gear: feat: exercicios MOD-4_SEC-1-D3
    :speaking_head: adicionando exercicios do dia 3, secão 1, modulo 4
    
    Refs: #MOD4

--- MOD-4/MOD-4-SEC_1/SECAO_1_3/exercicios/test_exercicio_1.py
+++ MOD-4/MOD-4-SEC_1/SECAO_1_3/exercicios/test_exercicio_1.py
@@ -1,9 +1,9 @@
-from exercicio import fizzbuzz
+from exercicio_1 import fizzbuzz
 
 
 def test_fizzbuzz_should_return_list_of_numbers():
     assert fizzbuzz(2) == [1, 2]
-     
+
 
 def test_fizzbuzz_divisible_by_three_should_be_fizz():
     assert fizzbuzz(3)[-1] == "Fizz"
