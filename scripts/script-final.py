8a828e91b58a5d05a94210ad0972e57badf5596e
Francisco Fernandes de Oliveira Junior <92826873+DeoliveiraJR@users.noreply.github.com>
2023-08-17 14:08:13 +0000

    :gear: feat: ultimas alterações
    :speaking_head: regra do exterior door para o glassdoor
    
    #EXTERIOR_ENV

--- scripts/script-final.py
+++ scripts/script-final.py
@@ -13,12 +13,12 @@ def apply_rules_to_fenestration_surfaces(building_surfaces_data, fenestration_su
             for building_surface in building_surfaces_data:
                 if building_surface['Name'] == building_surface_name:
                     zone_name = building_surface['Zone Name']
-                    break
+                    breaks
 
             if zone_name is not None:
-                if 'sala' in zone_name and surface_type == 'Door':
+                if 'sala' in zone_name and surface_type == 'GlassDoor':
                     fenestration_surface['Name'] = 'Pvs-' + fenestration_surface['Name']
-                elif 'quarto' in zone_name and surface_type == 'Door':
+                elif 'quarto' in zone_name and surface_type == 'GlassDoor':
                     fenestration_surface['Name'] = 'Pvq-' + fenestration_surface['Name']
                 elif 'sala' in zone_name and surface_type == 'Window':
                     fenestration_surface['Name'] = 'js-' + fenestration_surface['Name']
